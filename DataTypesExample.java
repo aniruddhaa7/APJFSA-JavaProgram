824d426cab70b7a79b215a4ff63d673fbc3b2fa7
aniruddhaa7 <aniruddha.vinchurkar@gmail.com>
2024-10-30 14:27:23 +0530

    Add files via upload

---
+++ DataTypesExample.java
@@ -0,0 +1,38 @@
+package APJSC9550;
+
+public class DataTypesExample {
+	public static void main(String[] args) {
+        int i = 30;
+        long l = 100000L;
+        byte b = 10;
+        short s = 20;
+        float f = 3.14f;
+        double d = 3.14159;
+        char c = 'A';
+        boolean bool = true;
+        String str = "Hello";
+
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + bool);
+        System.out.println("String: " + str);
+    }
+
+}
+//OUTPUT
+/*
+Int: 30
+Long: 100000
+Byte: 10
+Short: 20
+Float: 3.14
+Double: 3.14159
+Char: A
+Boolean: true
+String: Hello 
+*/
